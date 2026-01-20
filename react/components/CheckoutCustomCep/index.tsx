032c3e31c2ac36680bc59808fcd90e8ff90932a8
Camila Souza <camifstack@gmail.com>
2026-01-20 11:18:17 -0300

    fix: checkout improvement

--- react/components/CheckoutCustomCep/index.tsx
+++ react/components/CheckoutCustomCep/index.tsx
@@ -5,8 +5,10 @@ const CheckoutCustomCep: FC = () => {
   const [postalCode, setPostalCode] = React.useState('')
   const [postalCodeFilled, setPostalCodeFilled] = React.useState(false)
   const [errorModal, setErrorModal] = React.useState(false)
+  const [shippingData, setShippingData] = React.useState<any>(null)
 
   const handleInputCep = (e: React.ChangeEvent<HTMLInputElement>) => {
+
     let value = e.target.value
 
     value = value.replace(/\D/g, '')
@@ -52,89 +54,123 @@ const CheckoutCustomCep: FC = () => {
   }
 
   const updateShippingData = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const checkRegionServed = await fetch(
-      `/api/checkout/pub/regions?country=BRA&postalCode=${postalCode}`
-    )
+  e.preventDefault()
 
-    if (!checkRegionServed.ok) {
-      throw new Error(`HTTP error ${checkRegionServed.status}`)
-    }
-    const sellersByRegion: ISellers[] = await checkRegionServed.json()
-    const hasSellers = sellersByRegion[0].sellers.length > 0
-    if (!hasSellers) {
-      setErrorModal(true)
-      return
-    }
-    //@ts-ignore
-    const orderFormId = window?.vtexjs?.checkout?.orderFormId
+  const checkRegionServed = await fetch(
+    `/api/checkout/pub/regions?country=BRA&postalCode=${postalCode}`
+  )
 
-    const myHeaders = new Headers()
-    myHeaders.append('Content-Type', 'application/json')
-
-    const raw = JSON.stringify({
-      selectedAddresses: [
-        {
-          addressType: 'residential',
-          country: 'BRA',
-          postalCode: postalCode,
-        },
-      ],
-      clearAddressIfPostalCodeNotFound: true,
-      logisticsInfo: [
-        {
-          itemIndex: 0,
-          selectedDeliveryChannel: 'delivery',
-          selectedSla: 'ENTREGA CARAPRETA',
-        },
-      ],
-    })
-
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow' as RequestRedirect,
-    }
-
-    await fetch(
-      `/api/checkout/pub/orderForm/${orderFormId}/attachments/shippingData`,
-      requestOptions
-    )
-      .then(response => response.text())
-      .then(result => {
-        //@ts-ignore
-        vtexjs.checkout.getOrderForm().done(function(orderForm: any) {})
-
-        window?.localStorage?.setItem('userEnteredZipCode', 'true')
-
-        const bodyElement = document.querySelector('body')
-        bodyElement?.classList.remove('zip-code-not-entered')
-        setPostalCodeFilled(true)
-      })
-      .catch(error => console.error('erro no cep', error))
+  if (!checkRegionServed.ok) {
+    throw new Error(`HTTP error ${checkRegionServed.status}`)
   }
+
+  const sellersByRegion: ISellers[] = await checkRegionServed.json()
+  const hasSellers = sellersByRegion[0]?.sellers?.length > 0
+
+  if (!hasSellers) {
+    setErrorModal(true)
+    return
+  }
+
+  //@ts-ignore
+  const orderFormId = window?.vtexjs?.checkout?.orderFormId
+  if (!orderFormId) return
+
+  const response = await fetch(
+    `/api/checkout/pub/orderForm/${orderFormId}/attachments/shippingData`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        selectedAddresses: [
+          {
+            addressType: 'residential',
+            country: 'BRA',
+            postalCode,
+          },
+        ],
+        clearAddressIfPostalCodeNotFound: true,
+      }),
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error('Erro ao atualizar shippingData')
+  }
+
+  const updatedOrderForm = await response.json()
+
+  setShippingData(updatedOrderForm)
+
+  //@ts-ignore
+  vtexjs.checkout.getOrderForm().done(() => {})
+
+  window.localStorage.setItem('savedPostalCode', postalCode)
+  window.localStorage.setItem('userEnteredZipCode', 'true')
+
+  document.body.classList.remove('zip-code-not-entered')
+  setPostalCodeFilled(true)
+}
+
 
   const resetPostalCode = () => {
-    //@ts-ignore
-    resetShippingData(window?.vtexjs?.checkout?.orderFormId)
-    window?.localStorage?.setItem('userEnteredZipCode', 'false')
-    const bodyElement = document.querySelector('body')
-    bodyElement?.classList.add('zip-code-not-entered')
-    setPostalCodeFilled(false)
-    setPostalCode('')
-  }
+  //@ts-ignore
+  resetShippingData(window?.vtexjs?.checkout?.orderFormId)
+
+  window.localStorage.setItem('userEnteredZipCode', 'false')
+  window.localStorage.removeItem('savedPostalCode')
+
+  document.body.classList.add('zip-code-not-entered')
+
+  setPostalCodeFilled(false)
+  setPostalCode('')
+  setShippingData(null)
+}
 
   useEffect(() => {
-    const userEnteredZipCode =
-      window?.localStorage?.getItem('userEnteredZipCode') == 'true'
+  const userEnteredZipCode =
+    window.localStorage.getItem('userEnteredZipCode') === 'true'
 
-    if (!userEnteredZipCode) {
-      //@ts-ignore
-      const orderFormId = window?.vtexjs?.checkout?.orderFormId
-      resetShippingData(orderFormId)
-    }
-  }, [])
+  const savedPostalCode =
+    window.localStorage.getItem('savedPostalCode')
+
+  if (userEnteredZipCode && savedPostalCode) {
+    setPostalCode(savedPostalCode)
+    setPostalCodeFilled(true)
+    //@ts-ignore
+    vtexjs.checkout.getOrderForm().done((orderForm: any) => {
+      setShippingData(orderForm)
+    })
+  } else {
+    //@ts-ignore
+    const orderFormId = window?.vtexjs?.checkout?.orderFormId
+    resetShippingData(orderFormId)
+  }
+}, [])
+
+  const availableSla =
+  shippingData?.shippingData?.logisticsInfo?.[0]?.slas?.[1]
+
+  const parseShippingEstimate = (estimate: string) => {
+  const value = parseInt(estimate, 10)
+
+  if (estimate.endsWith('bd')) {
+    return `Até ${value} dia${value > 1 ? 's' : ''} útil${value > 1 ? 'eis' : ''}`
+  }
+
+  if (estimate.endsWith('d')) {
+    return `Até ${value} dia${value > 1 ? 's' : ''} útil${value > 1 ? 'eis' : ''}`
+  }
+
+  if (estimate.endsWith('h')) {
+    return `Até ${value} hora${value > 1 ? 's' : ''}`
+  }
+
+  return estimate
+}
+  console.log({ availableSla })
 
   return (
     <>
@@ -150,14 +186,29 @@ const CheckoutCustomCep: FC = () => {
                 <span className="custom-cep-span">Alterar</span>
               </button>
             </div>
+            {availableSla && (
+              <div className='custom-shipping-sla'>
+                <div className='custom-shipping-name-and-date'>
+                  <span className='custom-shipping-name'>{availableSla.name} </span>
+                    <span className='custom-shipping-date'>{parseShippingEstimate(availableSla.shippingEstimate)} </span>
+                  </div> 
+                  <p className='custom-shipping-price'>
+                    {(availableSla.price / 100).toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })}
+                  </p>
+              </div>
+              )}
           </div>
         ) : (
+          <div>
           <form className="custom-cep-form" onSubmit={updateShippingData}>
             <label
               htmlFor="custom-ship-postalCode"
               className="custom-cep-label"
             >
-              Digite o CEP para finalizar a compra*
+              1. Digite o CEP para finalizar a compra*
             </label>
             <div className="custom-cep-container-input">
               <input
@@ -182,6 +233,7 @@ const CheckoutCustomCep: FC = () => {
               Não sei meu CEP
             </a>
           </form>
+        </div>
         )}
       </div>
       {errorModal && (
@@ -206,6 +258,7 @@ const CheckoutCustomCep: FC = () => {
               Tentar outro CEP
             </button>
           </div>
+
         </div>
       )}
     </>
